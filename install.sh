a4fed5cf92f9ff71a76d189e841dd3d64d8a7bce
Gabriel Malaquias <5194287+GabrielMalakias@users.noreply.github.com>
2021-11-13 17:25:32 +0100

    Improving things

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 ln -fs ~/workspace/tools/myolnir/system/.vimrc ~/.vimrc
 ln -fs ~/workspace/tools/myolnir/.bash_profile ~/.bash_profile
 ln -fs ~/workspace/tools/myolnir/runcom/.bashrc ~/.bashrc
-ln -fs ~/workspace/tools/myolnir/runcom/.zshrc ~/.zshrc
 ##ln -fs ./git/.gitconfig ~/.git/gitconfig
 ln -fs ~/workspace/tools/myolnir/system/.alias ~/.bash_aliases
 
--- runcom/.bashrc
+++ runcom/.bashrc
@@ -1,9 +1,1 @@
 exec zsh
-
-#Workaround preforking
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-
-[ -f ~/.fzf.bash ] && source ~/.fzf.bash
-export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"
-source "$HOME/.cargo/env"
-. "$HOME/.cargo/env"
